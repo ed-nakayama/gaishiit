38905c0b27182fc3be423ea3aac7ac8043950c6d
ed-nakayama <nakayama@e-d.co.jp>
2025-03-11 11:51:05 +0900

    配信停止案内追加

--- resources/views/mail_templates/job_to_user.blade.php
+++ resources/views/mail_templates/job_to_user.blade.php
@@ -1,10 +1,14 @@
 {!! $user->name !!}様
 
 {!! $user->name !!}様が設定された条件に一致したジョブが見つかりました。
-下記URLよりご確認ください。
+詳細は下記URLよりご確認ください。
 
 @foreach ($jobList as $job)
 {{ url('/') }}/company/{{ $job->getCompanyId() }}/{{ $job->id }}
 @endforeach
+
+配信が不要な方はこちら
+{{ url('/') }}/setting
+からメール受信設定の変更をお願いします。
 
 @include('mail_templates.sign')
