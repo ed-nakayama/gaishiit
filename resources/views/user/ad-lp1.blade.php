1f67c6a0d957274f722891ca11c7c803dc47fb65
ed-nakayama <nakayama@e-d.co.jp>
2025-06-11 14:51:47 +0900

    google tag 追加

--- resources/views/user/ad-lp1.blade.php
+++ resources/views/user/ad-lp1.blade.php
@@ -1,5 +1,14 @@
 <html lang="ja">
   <head>
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NPVBH5VW');</script>
+<!-- End Google Tag Manager -->
+
+<!-- Google Tag Manager -->
   <link rel="stylesheet" type="text/css" href="/css/ad-lp.css">
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,6 +19,12 @@
   <script src="/js/ad-lp.js"></script>
   </head>
 <body class="ad-lp1">
+
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NPVBH5VW"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
 <!--――――――――――――――――――――――――――――――――――アイキャッチ――――――――――――――――――――――――――――――― -->
 <section class="ad-lp-eye-catch">
  <div class="ad-lp-eye-inner">
