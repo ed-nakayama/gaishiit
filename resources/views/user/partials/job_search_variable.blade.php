ec90509b78e13beb2f12f680c93d19f92ff49a2a
ed-nakayama <nakayama@e-d.co.jp>
2026-01-26 20:35:30 +0900

    バグ修正

--- resources/views/user/partials/job_search_variable.blade.php
+++ resources/views/user/partials/job_search_variable.blade.php
@@ -14,7 +14,7 @@
 		$cat             = \app\Models\JobCatDetail::where('id', $param['job_cat_details'])->where('del_flag','0')->first();
 		$valJobCat       = \app\Models\JobCat::where('id', $cat->job_cat_id)->where('del_flag','0')->orderBy('order_num')->orderBy('id')->get();
 		$valJobCatDetail = \app\Models\JobCatDetail::where('job_cat_id', $cat->job_cat_id)->where('del_flag','0')->orderBy('order_num')->orderBy('id')->get();
-		$job_cat_id = $cat->job_cat_id;
+		if (!empty($cat->job_cat_id)) $job_cat_id = $cat->job_cat_id;
 	} else {
 		$valJobCat       = \app\Models\JobCat::where('del_flag','0')->orderBy('order_num')->orderBy('id')->get();
 		$valJobCatDetail = \app\Models\JobCatDetail::where('del_flag','0')->orderBy('order_num')->orderBy('id')->get();
